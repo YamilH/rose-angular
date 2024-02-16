88acdc22412b3e9fdc2ccea4d2ab5b56c0a73023
YamilH <yamilhamui@gmail.com>
2024-02-16 18:03:36 -0300

    update-feb

--- src/app/app-navigation/app-navigation.component.ts
+++ src/app/app-navigation/app-navigation.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject, AfterViewInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Router, NavigationEnd } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -8,7 +9,7 @@ import { map, shareReplay } from 'rxjs/operators';
   templateUrl: './app-navigation.component.html',
   styleUrls: ['./app-navigation.component.scss']
 })
-export class AppNavigationComponent {
+export class AppNavigationComponent implements AfterViewInit {
   private breakpointObserver = inject(BreakpointObserver);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -16,4 +17,10 @@ export class AppNavigationComponent {
       map(result => result.matches),
       shareReplay()
     );
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    // Add the code for changing toolbar color here after the video ends
+  }
 }
